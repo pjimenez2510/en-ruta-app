bf4b05bc044db9da4fe5ccd18a683f597a8520d8
Alex <alexsanty10lm@gmail.com>
2025-06-08 14:08:38 -0500

    feat(CU-86a97bx1a): Changes in the seat-type interface

---
+++ src/features/seating/interfaces/seat-type.interface.ts
@@ -0,0 +1,10 @@
+export interface SeatType {
+    id: number;
+    nombre: string;
+    descripcion: string;
+    factorPrecio: number;
+    activo: boolean;
+    color: string;
+    icono: string;
+    tenantId: string;
+}
