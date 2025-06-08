7258ae9dee40c0490595f83927e1b401106a0687
Alex <alexsanty10lm@gmail.com>
2025-06-08 15:17:58 -0500

    feat(CU-86a99m2md): Creating form view for seat types

---
+++ src/features/seating/components/seat-type-form.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+} from "@/components/ui/form";
+import { SeatType } from "../interfaces/seat-type.interface";
+import { useSeatTypeForm } from "../hooks/use-seat-types-form";
+import { SeatTypeFormValues } from "../interfaces/form-schema";
+import { FormFields } from "./seat-type-form-fields";
+
+interface SeatTypeFormProps {
+  initialData?: SeatType;
+  onSubmit: (data: any) => void;
+  onCancel: () => void;
+}
+
+export const SeatTypeForm = ({ initialData, onSubmit, onCancel }: SeatTypeFormProps) => {
+  const { form, tenantId } = useSeatTypeForm(initialData);
+
+  const handleSubmit = (values: SeatTypeFormValues) => {
+    if (!initialData && tenantId === null) {
+      console.error("Tenant ID no disponible");
+      return;
+    }
+
+    const submitData = initialData
+      ? { ...values, tenantId: initialData.tenantId, activo: initialData.activo }
+      : values;
+
+    onSubmit(submitData);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-6">
+        <FormFields control={form.control} />
+        <div className="flex justify-end space-x-4">
+          <Button type="button" variant="outline" onClick={onCancel}>
+            Cancelar
+          </Button>
+          <Button type="submit">
+            {initialData ? "Actualizar" : "Crear"}
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+}; 
