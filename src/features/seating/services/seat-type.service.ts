cb2c436c2da842088871eaf8baccab04756a1071
Alex <alexsanty10lm@gmail.com>
2025-06-08 14:11:31 -0500

    feat(CU-86a97bx1a): Change of the name of the seat type service and updating endpoints

---
+++ src/features/seating/services/seat-type.service.ts
@@ -0,0 +1,76 @@
+import axios from 'axios';
+import type { AxiosInstance } from 'axios';
+import { SeatType } from '../interfaces/seat-type.interface';
+
+const api: AxiosInstance = axios.create({
+    baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const getAll = async (token: string | null = null): Promise<SeatType[]> => {
+    try {
+        const response = await api.get('/tipo-asientos',);
+        return response.data.data;
+    } catch (error) {
+        handleError(error);
+        return [];
+    }
+};
+
+export const getById = async (id: number, token: string | null = null): Promise<SeatType | null> => {
+    try {
+        const response: any = await api.get(`/tipo-asientos/${id}`,);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+        return null;
+    }
+};
+
+export const createSeatingType = async (seatingTypeData: any, token: string | null = null): Promise<SeatType | null> => {
+    try {
+        const response: any = await api.post('/tipo-asientos', seatingTypeData
+        );
+        return response.data;
+    } catch (error) {
+        handleError(error);
+        return null;
+    }
+};
+
+export const updateSeatingType = async (seatingTypeData: any, id: number, token: string | null = null): Promise<SeatType | null> => {
+    try {
+        const response: any = await api.put(`/tipo-asientos/${id}`, seatingTypeData);
+        return response.data;
+    } catch (error) {
+        handleError(error);
+        return null;
+    }
+};
+
+export const deactivateSeatType = async (id: number, token: string | null = null): Promise<boolean> => {
+    try {
+        await api.delete(`/tipo-asientos/${id}`);
+        return true;
+    } catch (error) {
+        handleError(error);
+        return false;
+    }
+};
+
+const handleError = (error: unknown): void => {
+    if (axios.isAxiosError(error)) {
+        console.error('Error en la solicitud', {
+            message: error.message,
+            code: error.code,
+            status: error.response?.status,
+            data: error.response?.data,
+        });
+    } else {
+        console.error('Error desconocido:', error);
+    }
+};
+
+export { api as seatingTypesApi };
