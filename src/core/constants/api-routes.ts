eb57295a01c06d6e9a2ecec2e481eca175c5a27f
Oscar Joel Ramírez Manzano <oramirez0004@uta.edu.ec>
2025-06-14 13:15:20 -0500

    update api routes with tennants and config_tenant

--- src/core/constants/api-routes.ts
+++ src/core/constants/api-routes.ts
@@ -17,4 +17,18 @@ export const API_ROUTES = {
     UPDATE_PROFILE: "/user/profile",
     CHANGE_PASSWORD: "/user/change-password",
   },
+  TENANTS: {
+    GET_TENANTS: "/tenants",
+    POST_TENANT: "/tenants",
+    GET_TENANT: "/tenants/:id",
+    UPDATE_TENANT: "/tenants/:id",
+    DELETE_TENANT: "/tenants/:id",
+  },
+  CONFIG_TENANT: {
+    GET_CONFIG_TENANT:"/configuraciones-tenant",
+    POST_CONFIG_TENANT:"/configuraciones-tenant",
+    GET_CONFIG_TENANT_BY_ID:"/configuraciones-tenant/:id",
+    UPDATE_CONFIG_TENANT:"/configuraciones-tenant/:id",
+    DELETE_CONFIG_TENANT:"/configuraciones-tenant/:id",
+  },
 };
---
+++ src/features/config-tenant/views/configuration_tenant.view.tsx
@@ -0,0 +1,16 @@
+import { ConfiguracionForm } from "../components/configuartion-form";
+
+export default function ConfiguracionView() {
+  return (
+    <div className="space-y-6">
+      <div>
+        <h1 className="text-2xl font-bold tracking-tight">Configuración</h1>
+        <p className="text-muted-foreground">
+          Personalice la configuración de su cooperativa.
+        </p>
+      </div>
+
+      <ConfiguracionForm />
+    </div>
+  );
+}
